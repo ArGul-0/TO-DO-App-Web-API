e507f3892674f153fff4a4672a6b5c9b20f469c2
ArGul <argul.mail@gmail.com>
2026-04-21 10:28:17 +0300

    Added User Entity and Email Value Object.

---
+++ ToDoApp.Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using ToDoApp.Domain.ValueObjects;
+
+namespace ToDoApp.Domain.Entities
+{
+    internal class User
+    {
+        public int Id { get; }
+        public required string Username { get; set; }
+        public required Email Email { get; set; }
+        public required string HashedPassword { get; set; }
+
+        public void UpdateUsername(string newUsername)
+        {
+            if(string.IsNullOrEmpty(newUsername)) 
+                throw new ArgumentException("Username cannot be null or empty.", nameof(newUsername));
+
+            Username = newUsername;
+        }
+
+        public void UpdateEmail(Email newEmail)
+        {
+            if(newEmail == null) 
+                throw new ArgumentNullException(nameof(newEmail), "Email cannot be null.");
+
+            Email = newEmail;
+        }
+    }
+}
---
+++ ToDoApp.Domain/ValueObjects/Email.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Mail;
+
+namespace ToDoApp.Domain.ValueObjects
+{
+    internal class Email
+    {
+        public string Value { get; }
+        public Email(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("Email cannot be null or empty.", nameof(value));
+            else if (!IsValidEmail(value))
+                throw new ArgumentException("Invalid email format.", nameof(value));
+
+            Value = value;
+        }
+
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
