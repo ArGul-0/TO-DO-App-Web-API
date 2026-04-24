989fb36172321b332afa4662b991db0ba6af5b22
ArGul <argul.mail@gmail.com>
2026-04-24 20:18:21 +0300

    Created IJwtTokenGenerator interface.

---
+++ ToDoApp.Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Application.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        public string GenerateAccessToken(User user);
+
+    }
+}
