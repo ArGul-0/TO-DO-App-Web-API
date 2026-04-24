62ccbc23de3a1e352afff48cb879f53ef7949af9
ArGul <argul.mail@gmail.com>
2026-04-24 20:20:21 +0300

    Added comments for IJwtTokenGenerator.

--- ToDoApp.Application/Interfaces/IJwtTokenGenerator.cs
+++ ToDoApp.Application/Interfaces/IJwtTokenGenerator.cs
@@ -4,6 +4,11 @@ namespace ToDoApp.Application.Interfaces
 {
     public interface IJwtTokenGenerator
     {
+        /// <summary>
+        /// Generates a JWT access token for the given user. The token typically includes claims such as the user's ID, username, and email, and is signed using a secret key to ensure its integrity and authenticity.
+        /// </summary>
+        /// <param name="user" >The user for whom the token is being generated.</param>
+        /// <returns>The generated JWT access token as a string.</returns>
         public string GenerateAccessToken(User user);
 
     }
