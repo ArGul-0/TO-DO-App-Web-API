6f53bbc0feaaee3572c4a48e05a6b160b0f3f211
ArGul <argul.mail@gmail.com>
2026-04-24 19:37:17 +0300

    Realised Result Pattern and added Repositories folder in Infrastructure layer.

---
+++ ToDoApp.Application/Common/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoApp.Application.Common
+{
+    internal class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error { get; }
+
+        protected Result(bool isSuccess, Error error)
+        {
+            if (isSuccess && error != Error.None)
+                throw new InvalidOperationException("A successful result cannot have an error.");
+            if (!isSuccess && error == Error.None)
+                throw new InvalidOperationException("A failure result must have an error.");
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success() => new(true, Error.None);
+        public static Result Failure(Error error) => new(false, error);
+    }
+}
---
+++ ToDoApp.Application/Common/ResultT.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoApp.Application.Common
+{
+    internal class ResultT<T> : Result
+    {
+        public T Value { get; }
+        protected ResultT(T value, bool isSuccess, Error error) : base(isSuccess, error)
+        {
+            Value = value;
+        }
+
+        public static ResultT<T> Success(T value) => new(value, true, Error.None);
+        public static ResultT<T> Failure(Error error) => new(default!, false, error);
+    }
+}
