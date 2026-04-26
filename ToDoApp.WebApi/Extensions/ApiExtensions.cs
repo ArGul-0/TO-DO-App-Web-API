e4d2ffb525f211ab85b74a2f2cd1670a6b8de7ec
ArGul <argul.mail@gmail.com>
2026-04-26 14:48:06 +0300

    Now AddAuthentication and AddAuthorization in ApiExtensions.

--- ToDoApp.WebApi/Extensions/ApiExtensions.cs
+++ ToDoApp.WebApi/Extensions/ApiExtensions.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.OpenApi;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using Serilog;
 using Serilog.Events;
+using System.Text;
+using ToDoApp.Infrastructure.Authentication;
 
 namespace ToDoApp.WebApi.Extensions
 {
@@ -57,6 +61,46 @@ namespace ToDoApp.WebApi.Extensions
                 .CreateLogger();
 
             builder.Host.UseSerilog();
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder AddAuth(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) // Add Authentication Services With JWT Bearer Scheme
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    var jwtOptions = builder.Configuration.GetSection("JwtOptions").Get<JwtOptions>() ?? throw new InvalidOperationException("JwtOptions configuration is missing.");
+
+                    options.TokenValidationParameters = new TokenValidationParameters // Configure Token Validation Parameters
+                    {
+                        ValidateIssuer = true, // Enable Issuer Validation To Ensure Token Is Issued By A Trusted Authority
+                        ValidIssuer = jwtOptions.Issuer, // Set The Valid Issuer To The Value From Configuration
+                        ValidateAudience = true, // Enable Audience Validation To Ensure Token Is Intended For This API
+                        ValidAudience = jwtOptions.Audience, // Set The Valid Audience To The Value From Configuration
+                        ValidateLifetime = true, // Enable Lifetime Validation To Ensure Tokens Expire
+                        ClockSkew = TimeSpan.Zero, // Set Clock Skew To Zero To Prevent Delayed Expiration Issues
+                        ValidateIssuerSigningKey = true, // Enable Issuer Signing Key Validation To Ensure Token Integrity
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                            jwtOptions.SecretKey!)), // Use A Symmetric Security Key Derived From The Secret Key In Configuration
+                    };
+
+                    options.Events = new JwtBearerEvents // Configure JWT Bearer Events For Better Error Handling
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var token = context.Request.Cookies[jwtOptions.NameInCookies!];
+
+                            if (!string.IsNullOrEmpty(token))
+                            {
+                                context.Token = token;
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            builder.Services.AddAuthorization();
 
             return builder;
         }
--- ToDoApp.WebApi/Program.cs
+++ ToDoApp.WebApi/Program.cs
@@ -1,8 +1,5 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.CookiePolicy;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
-using System.Text;
 using ToDoApp.Application.Interfaces;
 using ToDoApp.Infrastructure.Authentication;
 using ToDoApp.Infrastructure.DependencyInjection;
@@ -26,45 +23,12 @@ namespace ToDoApp.WebApi
 
             builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
 
+            builder.AddAuth();
+
             builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) // Add Authentication Services With JWT Bearer Scheme
-            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-            {
-                var jwtOptions = builder.Configuration.GetSection("JwtOptions").Get<JwtOptions>() ?? throw new InvalidOperationException("JwtOptions configuration is missing.");
-
-                options.TokenValidationParameters = new TokenValidationParameters // Configure Token Validation Parameters
-                {
-                    ValidateIssuer = true, // Enable Issuer Validation To Ensure Token Is Issued By A Trusted Authority
-                    ValidIssuer = jwtOptions.Issuer, // Set The Valid Issuer To The Value From Configuration
-                    ValidateAudience = true, // Enable Audience Validation To Ensure Token Is Intended For This API
-                    ValidAudience = jwtOptions.Audience, // Set The Valid Audience To The Value From Configuration
-                    ValidateLifetime = true, // Enable Lifetime Validation To Ensure Tokens Expire
-                    ClockSkew = TimeSpan.Zero, // Set Clock Skew To Zero To Prevent Delayed Expiration Issues
-                    ValidateIssuerSigningKey = true, // Enable Issuer Signing Key Validation To Ensure Token Integrity
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                        jwtOptions.SecretKey!)), // Use A Symmetric Security Key Derived From The Secret Key In Configuration
-                };
-
-                options.Events = new JwtBearerEvents // Configure JWT Bearer Events For Better Error Handling
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var token = context.Request.Cookies[jwtOptions.NameInCookies!];
-
-                        if (!string.IsNullOrEmpty(token))
-                        {
-                            context.Token = token;
-                        }
-
-                        return Task.CompletedTask;
-                    }
-                };
-            });
-            builder.Services.AddAuthorization();
 
             var app = builder.Build();
 
