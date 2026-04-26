3958017db773a7334ecb7681bb286e23109472f3
ArGul <argul.mail@gmail.com>
2026-04-26 15:04:56 +0300

    Fix syntax errors.

--- ToDoApp.WebApi/Endpoints/AuthEndpoints.cs
+++ ToDoApp.WebApi/Endpoints/AuthEndpoints.cs
@@ -8,9 +8,10 @@ namespace ToDoApp.WebApi.Endpoints
         {
             var authGroup = app.MapGroup("/Auth"); // Create A Group For /Auth Endpoints
 
-            return authGroup;
-
             authGroup.MapGet("/Test", () => "Auth Endpoint Is Working!").WithTags("Auth"); // Example Test Endpoint
+
+            return authGroup;
         }
+
     }
 }
