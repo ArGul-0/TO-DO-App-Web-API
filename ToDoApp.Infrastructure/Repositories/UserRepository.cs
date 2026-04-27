da0498cabc00b43cc37d529e6a63f5ccca3340b6
ArGul <argul.mail@gmail.com>
2026-04-27 15:14:57 +0300

    Created minimal UserRepository.

---
+++ ToDoApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using ToDoApp.Application.Interfaces;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Infrastructure.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        public Task AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
