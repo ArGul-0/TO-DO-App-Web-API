800eb33995bae75cf4104e9aaef36d9e80ff87aa
ArGul <argul.mail@gmail.com>
2026-04-27 17:24:01 +0300

    Realised auth /test endpoint.

--- ToDoApp.WebApi/Endpoints/AuthEndpoints.cs
+++ ToDoApp.WebApi/Endpoints/AuthEndpoints.cs
@@ -1,16 +1,31 @@
 ﻿using ToDoApp.Application.UseCases.Users.CreateUser;
+using ToDoApp.WebApi.Extensions;
 
 namespace ToDoApp.WebApi.Endpoints
 {
     public static class AuthEndpoints
     {
+        const string LoginEndpointName = "Login"; // Constant For The Login Endpoint Name
+        const string RegisterEndpointName = "Register"; // Constant For The Register Endpoint Name
+
         public static RouteGroupBuilder MapAuthEndpoints(this WebApplication app)
         {
             var authGroup = app.MapGroup("/Auth"); // Create A Group For /Auth Endpoints
 
-            authGroup.MapPost("/Test", (CreateUserHandler createUserHandler) =>
-            {
 
+
+            authGroup.MapPost("/Test", async (CreateUserHandler createUserHandler, HttpContext httpContext, IConfiguration configuration) =>
+            {
+                var result = await createUserHandler.Handle(new CreateUserRequest(
+                    Username: "testuser",
+                    Email: "testEmail@gmail.com",
+                    Password: "TestPassword123"
+                    ));
+
+                httpContext.Response.Cookies
+                .Append(configuration["JwtOptions:NameInCookies"]!, result.Value.ToString()); // Set The JWT Token In Cookies
+
+                return Results.Ok(result);
             }).WithName("TestAuthEndpoint");
 
             return authGroup;
