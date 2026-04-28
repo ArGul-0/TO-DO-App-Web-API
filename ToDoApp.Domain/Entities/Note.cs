de7d0ee872cbcd9b9a7cf153ecba6f65f6d6ead1
ArGul <argul.mail@gmail.com>
2026-04-28 12:22:23 +0300

    Createad Note

---
+++ ToDoApp.Domain/Entities/Note.cs
@@ -0,0 +1,22 @@
+﻿namespace ToDoApp.Domain.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string Content { get; set; } = string.Empty;
+
+        public void UpdateTitle(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+
+            Title = title;
+        }
+
+        public void UpdateContent(string content)
+        {
+            Content = content ?? string.Empty; // Allow content to be empty but not null
+        }
+    }
+}
