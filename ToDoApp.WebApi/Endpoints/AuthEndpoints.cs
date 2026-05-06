486c9f5637a90c74409482dbaddc82168b7bd3ce
ArGul <argul.mail@gmail.com>
2026-05-06 16:12:44 +0300

    Realised Login endpoint.

--- ToDoApp.WebApi/Endpoints/AuthEndpoints.cs
+++ ToDoApp.WebApi/Endpoints/AuthEndpoints.cs
@@ -1,12 +1,13 @@
 ﻿using ToDoApp.Application.UseCases.Users.CreateUser;
+using ToDoApp.Application.UseCases.Users.LoginUser;
 using ToDoApp.WebApi.Extensions;
 
 namespace ToDoApp.WebApi.Endpoints
 {
     public static class AuthEndpoints
     {
-        const string LoginEndpointName = "Login"; // Constant For The Login Endpoint Name
         const string RegisterEndpointName = "Register"; // Constant For The Register Endpoint Name
+        const string LoginEndpointName = "Login"; // Constant For The Login Endpoint Name
 
         public static RouteGroupBuilder MapAuthEndpoints(this WebApplication app)
         {
@@ -30,6 +31,22 @@ namespace ToDoApp.WebApi.Endpoints
 
                 return Results.Ok(result.Value);
             }).WithName(RegisterEndpointName);
+
+            authGroup.MapPost("/Login", async (
+                LoginUserRequest request,
+                LoginUserHandler loginUserHandler,
+                HttpContext httpContext,
+                IConfiguration configuration) =>
+            {
+                var result = await loginUserHandler.Handle(request);
+                if (result.IsFailure)
+                    return result.ToHttpResult();
+
+                httpContext.Response.Cookies
+                .Append(configuration["JwtOptions:NameInCookies"]!, result.Value.ToString()); // Set The JWT Token In Cookies
+
+                return Results.Ok(result.Value);
+            }).WithName(LoginEndpointName);
 
             return authGroup;
         }
