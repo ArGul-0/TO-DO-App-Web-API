33050bac2c16976c9ec412df9e54e99850bc6631
ArGul <argul.mail@gmail.com>
2026-05-07 17:20:44 +0300

    Realised GetAllNotes handler.

--- ToDoApp.Application/UseCases/Notes/GetAllNotes/GetAllNotesHandler.cs
+++ ToDoApp.Application/UseCases/Notes/GetAllNotes/GetAllNotesHandler.cs
@@ -1,13 +1,33 @@
 ﻿using ToDoApp.Application.Common;
 using ToDoApp.Application.DTOs;
+using ToDoApp.Application.Interfaces;
 
 namespace ToDoApp.Application.UseCases.Notes.GetAllNotes
 {
     public class GetAllNotesHandler
     {
-        public Task<ResultT<List<NoteDto>>> Handle(int userId)
-        {
+        private readonly INotesRepository notesRepository;
 
+        public GetAllNotesHandler(INotesRepository notesRepository)
+        {
+            this.notesRepository = notesRepository;
+        }
+
+        public async Task<ResultT<List<NoteDto>>> Handle(int userId)
+        {
+            var notes = await notesRepository.GetAllNotesAsync(userId);
+
+            if(notes is null || !notes.Any())
+                return ResultT<List<NoteDto>>.Failure(NotesErrors.NotesNotFound);
+
+            var noteDtos = notes.Select(note => new NoteDto
+            (
+                Id:note.Id,
+                Title: note.Title,
+                Content: note.Content
+            )).ToList();
+
+            return ResultT<List<NoteDto>>.Success(noteDtos);
         }
     }
 }
---
+++ ToDoApp.Application/UseCases/Notes/NotesErrors.cs
@@ -0,0 +1,15 @@
+﻿using ToDoApp.Application.Common;
+
+namespace ToDoApp.Application.UseCases.Notes
+{
+    public static class NotesErrors
+    {
+        public static readonly Error NotesNotFound = new Error("NotesNotFound",
+            "No notes were found for the specified user.",
+            ErrorType.NotFound);
+
+        public static readonly Error NoteNotFound = new Error("NoteNotFound",
+            "The specified note was not found.",
+            ErrorType.NotFound);
+    }
+}
