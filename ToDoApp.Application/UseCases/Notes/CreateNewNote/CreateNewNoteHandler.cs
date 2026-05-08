d2ed26fafae2e85dad7af86c4f9451859ae912b3
ArGul <argul.mail@gmail.com>
2026-05-08 20:34:09 +0300

    Added logger in CreateNewNoteHandler.

--- ToDoApp.Application/UseCases/Notes/CreateNewNote/CreateNewNoteHandler.cs
+++ ToDoApp.Application/UseCases/Notes/CreateNewNote/CreateNewNoteHandler.cs
@@ -1,4 +1,5 @@
-﻿using ToDoApp.Application.Common;
+﻿using Microsoft.Extensions.Logging;
+using ToDoApp.Application.Common;
 using ToDoApp.Application.DTOs;
 using ToDoApp.Application.Interfaces;
 using ToDoApp.Application.Interfaces.Repositories;
@@ -9,11 +10,13 @@ namespace ToDoApp.Application.UseCases.Notes.CreateNewNote
     public class CreateNewNoteHandler
     {
         private readonly IUserRepository userRepository;
+        private readonly ILogger<CreateNewNoteHandler> logger;
         private readonly IUnitOfWork unitOfWork;
 
-        public CreateNewNoteHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        public CreateNewNoteHandler(IUserRepository userRepository, ILogger<CreateNewNoteHandler> logger, IUnitOfWork unitOfWork)
         {
             this.userRepository = userRepository;
+            this.logger = logger;
             this.unitOfWork = unitOfWork;
         }
 
@@ -22,11 +25,18 @@ namespace ToDoApp.Application.UseCases.Notes.CreateNewNote
             var user = await userRepository.GetUserWithNotesAsync(userId);
 
             if(user is null)
+            {
+                logger.LogWarning("User with id {UserId} not found when trying to create a new note", userId);
+
                 return ResultT<NoteDto>.Failure(UsersErrors.UserNotFound);
+            }
+                
 
             user.AddNote(request.Title, request.Content);
 
             await unitOfWork.SaveChangesAsync();
+
+            logger.LogInformation("New note created for user with id {UserId}", userId);
 
             var noteDto = new NoteDto(
                 Id: user.Notes.Last().Id,
