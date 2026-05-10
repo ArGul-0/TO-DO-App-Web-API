0011e267d2ed2a8b273667acf9c265599f41f638
ArGul <argul.mail@gmail.com>
2026-05-10 13:18:47 +0300

    Created UserMappings

---
+++ ToDoApp.Application/Common/Mappings/UserMappings.cs
@@ -0,0 +1,18 @@
+﻿using ToDoApp.Application.DTOs;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Application.Common.Mappings
+{
+    public static class UserMappings
+    {
+        public static UserDto ToDto(this User user)
+        {
+            return new UserDto(
+                Id: user.Id,
+                Username: user.Username,
+                Email: user.Email.Value,
+                Visibility: user.Visibility
+                );
+        }
+    }
+}
