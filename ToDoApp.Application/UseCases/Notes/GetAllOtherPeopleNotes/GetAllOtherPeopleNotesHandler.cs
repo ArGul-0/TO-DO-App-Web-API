08466eafc56196df793b620698cdf008d8a40729
ArGul <argul.mail@gmail.com>
2026-05-10 14:15:22 +0300

    Added notesRepository in GetAllOtherPeopleNotesHandler

--- ToDoApp.Application/UseCases/Notes/GetAllOtherPeopleNotes/GetAllOtherPeopleNotesHandler.cs
+++ ToDoApp.Application/UseCases/Notes/GetAllOtherPeopleNotes/GetAllOtherPeopleNotesHandler.cs
@@ -1,10 +1,17 @@
 ﻿using ToDoApp.Application.Common;
 using ToDoApp.Application.DTOs;
+using ToDoApp.Application.Interfaces.Repositories;
 
 namespace ToDoApp.Application.UseCases.Notes.GetAllOtherPeopleNotes
 {
     public class GetAllOtherPeopleNotesHandler
     {
+        private readonly INotesRepository notesRepository;
+        public GetAllOtherPeopleNotesHandler(INotesRepository notesRepository)
+        {
+            this.notesRepository = notesRepository;
+        }
+
         public Task<ResultT<NoteDto>> Handle()
         {
             return Task.FromResult(
