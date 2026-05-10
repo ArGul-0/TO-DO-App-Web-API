8836d9bc59841d8f7670f676b13bfa36b5087420
ArGul <argul.mail@gmail.com>
2026-05-10 12:13:32 +0300

    dotnet ef migrations add addedVisibilityForUser --project ToDoApp.Infrastructure --startup-project ToDoApp.WebApi

---
+++ ToDoApp.Infrastructure/Migrations/20260510091253_addedVisibilityForUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedVisibilityForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Visibility",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Visibility",
+                table: "Users");
+        }
+    }
+}
