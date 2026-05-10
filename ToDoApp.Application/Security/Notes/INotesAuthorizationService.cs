abd2c0c7f58387fb20899892c2325ccee862db05
ArGul <argul.mail@gmail.com>
2026-05-10 15:10:50 +0300

    Now we use notesAuthorizationService in GetAllNotesHandler

--- ToDoApp.Application/Security/Notes/INotesAuthorizationService.cs
+++ ToDoApp.Application/Security/Notes/INotesAuthorizationService.cs
@@ -5,5 +5,6 @@ namespace ToDoApp.Application.Security.Notes
     public interface INotesAuthorizationService
     {
         public bool CanSeeNote(User currentUser, Note note);
+        List<Note> FilterVisibleNotes(User currentUser, List<Note> notes);
     }
 }
--- ToDoApp.Application/UseCases/Notes/GetAllNotes/GetAllNotesHandler.cs
+++ ToDoApp.Application/UseCases/Notes/GetAllNotes/GetAllNotesHandler.cs
@@ -2,26 +2,39 @@
 using ToDoApp.Application.Common.Mappings;
 using ToDoApp.Application.DTOs;
 using ToDoApp.Application.Interfaces.Repositories;
+using ToDoApp.Application.Security.Notes;
+using ToDoApp.Application.UseCases.Users;
 
 namespace ToDoApp.Application.UseCases.Notes.GetAllNotes
 {
     public class GetAllNotesHandler
     {
         private readonly INotesRepository notesRepository;
+        private readonly IUserRepository userRepository;
+        private readonly INotesAuthorizationService notesAuthorizationService;
 
-        public GetAllNotesHandler(INotesRepository notesRepository)
+        public GetAllNotesHandler(INotesRepository notesRepository, IUserRepository userRepository, INotesAuthorizationService notesAuthorizationService)
         {
             this.notesRepository = notesRepository;
+            this.userRepository = userRepository;
+            this.notesAuthorizationService = notesAuthorizationService;
         }
 
         public async Task<ResultT<List<NoteDto>>> Handle(int userId)
         {
-            var notes = await notesRepository.GetAllNotesAsync(userId);
+            var notes = await notesRepository.GetAllNotesAsync();
 
-            if(notes is null || !notes.Any())
+            if (notes is null || !notes.Any())
                 return ResultT<List<NoteDto>>.Failure(NotesErrors.NotesNotFound);
 
-            var noteDtos = notes.Select(note => note.ToDto()).ToList();
+            var user = await userRepository.GetUserByIdAsync(userId);
+
+            if (user is null)
+                return ResultT<List<NoteDto>>.Failure(UsersErrors.UserNotFound);
+
+            var filteredNotes = notesAuthorizationService.FilterVisibleNotes(user, notes);
+
+            var noteDtos = filteredNotes.Select(filteredNotes => filteredNotes.ToDto()).ToList();
 
             return ResultT<List<NoteDto>>.Success(noteDtos);
         }
