5f5d4925dc132783d1ee762bdfd0c0de3a5be300
ArGul <argul.mail@gmail.com>
2026-05-12 19:44:14 +0300

    Added DI In DeleteUserNoteHandler

--- ToDoApp.Application/UseCases/Notes/DeleteUserNote/DeleteUserNoteHandler.cs
+++ ToDoApp.Application/UseCases/Notes/DeleteUserNote/DeleteUserNoteHandler.cs
@@ -1,9 +1,28 @@
 ﻿using ToDoApp.Application.Common;
+using ToDoApp.Application.Interfaces;
+using ToDoApp.Application.Interfaces.Repositories;
+using ToDoApp.Application.Security.Notes;
 
 namespace ToDoApp.Application.UseCases.Notes.DeleteUserNote
 {
     public class DeleteUserNoteHandler
     {
+        private readonly IUserRepository userRepository;
+        private readonly INotesRepository notesRepository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly INotesAuthorizationService notesAuthorizationService
+        public DeleteUserNoteHandler(
+            IUserRepository userRepository,
+            INotesRepository notesRepository,
+            IUnitOfWork unitOfWork,
+            INotesAuthorizationService notesAuthorizationService)
+        {
+            this.userRepository = userRepository;
+            this.notesRepository = notesRepository;
+            this.unitOfWork = unitOfWork;
+            this.notesAuthorizationService = notesAuthorizationService;
+        }
+
         public Task<Result> Handle(int userId)
         {
 
