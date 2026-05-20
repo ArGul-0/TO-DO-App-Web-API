c09260975af2fb62c0a16cfe6ab18076257cbc73
ArGul <argul.mail@gmail.com>
2026-05-20 14:01:03 +0300

    dotnet ef migrations add AddedIsDoneInNoteEntity. --project ToDoApp.Infrastructure --startup-project ToDoApp.WebApi

---
+++ ToDoApp.Infrastructure/Migrations/20260520110032_AddedIsDoneInNoteEntity..cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsDoneInNoteEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDone",
+                table: "Notes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDone",
+                table: "Notes");
+        }
+    }
+}
