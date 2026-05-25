4b16dc382a764682bc6da6b7dec0164df740e453
ArGul <argul.mail@gmail.com>
2026-05-25 15:18:08 +0300

    Added more dependensies in SendFriendRequestHandler

--- ToDoApp.Application/UseCases/Friends/SendFriendRequest/SendFriendRequestHandler.cs
+++ ToDoApp.Application/UseCases/Friends/SendFriendRequest/SendFriendRequestHandler.cs
@@ -1,4 +1,6 @@
-﻿using ToDoApp.Application.Common;
+﻿using Microsoft.Extensions.Logging;
+using ToDoApp.Application.Common;
+using ToDoApp.Application.Interfaces;
 using ToDoApp.Application.Interfaces.Repositories;
 
 namespace ToDoApp.Application.UseCases.Friends.SendFriendRequest
@@ -6,9 +8,19 @@ namespace ToDoApp.Application.UseCases.Friends.SendFriendRequest
     public class SendFriendRequestHandler
     {
         private readonly IFriendRepository friendRepository;
-        public SendFriendRequestHandler(IFriendRepository friendRepository)
+        private readonly IUserRepository userRepository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ILogger<SendFriendRequestHandler> logger;
+        public SendFriendRequestHandler(
+            IFriendRepository friendRepository,
+            IUserRepository userRepository,
+            IUnitOfWork unitOfWork,
+            ILogger<SendFriendRequestHandler> logger)
         {
             this.friendRepository = friendRepository;
+            this.userRepository = userRepository;
+            this.unitOfWork = unitOfWork;
+            this.logger = logger;
         }
 
         public async Task<Result> Handle(int userId, int friendId)
