791495c04feaa6a5aaff2023648782ba2104eb0b
ArGul <argul.mail@gmail.com>
2026-05-25 14:30:23 +0300

    Created minimal SendFriendRequestHandler.

---
+++ ToDoApp.Application/UseCases/Friends/SendFriendRequest/SendFriendRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using ToDoApp.Application.Common;
+
+namespace ToDoApp.Application.UseCases.Friends.SendFriendRequest
+{
+    public class SendFriendRequestHandler
+    {
+        public Task<ResultT<SendFriendRequestResponse>> Handle(SendFriendRequestRequest Request)
+        {
+
+        }
+    }
+}
