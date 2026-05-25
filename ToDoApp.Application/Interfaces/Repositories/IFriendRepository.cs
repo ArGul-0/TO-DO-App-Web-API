a26124969f1cc4c2fefc7dd4329bed78ab99e3a3
ArGul <argul.mail@gmail.com>
2026-05-25 14:49:37 +0300

    Created IFriendRepository and his minimal realization.

---
+++ ToDoApp.Application/Interfaces/Repositories/IFriendRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoApp.Application.Interfaces.Repositories
+{
+    public interface IFriendRepository
+    {
+        public Task SendFriendRequest(int requesterId, int addresseeId);
+    }
+}
---
+++ ToDoApp.Infrastructure/Repositories/FriendRepository.cs
@@ -0,0 +1,18 @@
+﻿using ToDoApp.Application.Interfaces.Repositories;
+
+namespace ToDoApp.Infrastructure.Repositories
+{
+    internal class FriendRepository : IFriendRepository
+    {
+        private readonly AppDbContext dbContext;
+        public FriendRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task SendFriendRequest(int requesterId, int addresseeId)
+        {
+
+        }
+    }
+}
