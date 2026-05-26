f6a443d471e0fc9b489ee65d8b729bb93e6a779b
ArGul <argul.mail@gmail.com>
2026-05-26 15:16:11 +0300

    Realised minimal SendFriendRequestHandler

--- ToDoApp.Application/UseCases/Friends/SendFriendRequest/SendFriendRequestHandler.cs
+++ ToDoApp.Application/UseCases/Friends/SendFriendRequest/SendFriendRequestHandler.cs
@@ -2,6 +2,7 @@
 using ToDoApp.Application.Common;
 using ToDoApp.Application.Interfaces;
 using ToDoApp.Application.Interfaces.Repositories;
+using ToDoApp.Domain.Entities;
 
 namespace ToDoApp.Application.UseCases.Friends.SendFriendRequest
 {
@@ -25,7 +26,13 @@ namespace ToDoApp.Application.UseCases.Friends.SendFriendRequest
 
         public async Task<Result> Handle(int userId, int friendId)
         {
+            var newFriendship = new Friendship(userId, friendId);
 
+            await friendRepository.AddFriendAsync(newFriendship);
+
+            await unitOfWork.SaveChangesAsync();
+
+            return Result.Success();
         }
     }
 }
