5982a07631e7b594e5ead125687687ad45b43892
ArGul <argul.mail@gmail.com>
2026-05-26 15:42:55 +0300

    Realised UpdateFriendshipAsync.

--- ToDoApp.Application/Interfaces/Repositories/IFriendRepository.cs
+++ ToDoApp.Application/Interfaces/Repositories/IFriendRepository.cs
@@ -5,6 +5,7 @@ namespace ToDoApp.Application.Interfaces.Repositories
     public interface IFriendRepository
     {
         public Task AddFriendshipAsync(Friendship friendship);
+        public Task UpdateFriendshipAsync(Friendship friendship);
         public Task<bool> FriendshipExistsAsync(int userId, int friendId);
         public Task RemoveFriendshipAsync(int userId, int friendId);
     }
