59f906116c12b2eaf20c72fdc6e4688941b8e38e
ArGul <argul.mail@gmail.com>
2026-05-29 19:01:38 +0300

    Created FriendshipDto

---
+++ ToDoApp.Application/DTOs/FriendshipDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToDoApp.Domain.Enums;
+
+namespace ToDoApp.Application.DTOs
+{
+    public record FriendshipDto(
+        [Required] int Id,
+        [Required] int RequesterId,
+        [Required] string RequesterUsername,
+        [Required] int AddresseeId,
+        [Required] string AddresseeUsername,
+        [Required] FriendshipStatus Status
+        );
+}
