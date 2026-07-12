cf44bdcb0bd348716094dcf909db7d6b8d2e3417
ArGul <argul.mail@gmail.com>
2026-07-12 10:36:02 +0300

    More documentation.

--- ToDoApp.Application/Interfaces/Repositories/INoteRepository.cs
+++ ToDoApp.Application/Interfaces/Repositories/INoteRepository.cs
@@ -4,8 +4,18 @@ namespace ToDoApp.Application.Interfaces.Repositories
 {
     public interface INoteRepository
     {
+        /// <summary>
+        /// Asynchronously retrieves a note by id without tracking.
+        /// </summary>
+        /// <param name="id">Note id.</param>
+        /// <returns>The note if found; otherwise null.</returns>
         public Task<Note?> GetNoteById(int id);
 
+        /// <summary>
+        /// Asynchronously retrieves a note by id with tracking.
+        /// </summary>
+        /// <param name="id">Note id.</param>
+        /// <returns>The tracked note if found; otherwise null.</returns>
         public Task<Note?> GetNoteByIdWithTracking(int id);
 
         /// <summary>
@@ -21,8 +31,25 @@ namespace ToDoApp.Application.Interfaces.Repositories
         /// <returns>A task that represents the asynchronous operation. The task result contains the note if it exists.</returns>
         public Task<Note?> GetNoteWithOwnerByIdAsync(int id);
 
+        /// <summary>
+        /// Asynchronously retrieves all notes for a user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <returns>List of notes (may be empty).</returns>
         public Task<List<Note>> GetAllNotesByUserIdAsync(int userId);
 
+        /// <summary>
+        /// Asynchronously adds a new note to the repository.
+        /// </summary>
+        /// <param name="note">The note to be added.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result indicates whether the operation was successful.</returns>
+        public Task<bool> AddNoteAsync(Note note);
+
+        /// <summary>
+        /// Asynchronously deletes a note from the repository.
+        /// </summary>
+        /// <param name="noteId">The id of the note to be deleted.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result indicates whether the operation was successful.</returns>
         public Task<bool> DeleteNoteAsync(int noteId);
     }
 }
