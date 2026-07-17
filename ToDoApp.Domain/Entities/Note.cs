ed9dace2e42400a76b75295f003544424a4c9597
ArGul <argul.mail@gmail.com>
2026-07-17 15:14:20 +0300

    Added CreatedAt and UpdatedAt in Note Entity.

--- ToDoApp.Domain/Entities/Note.cs
+++ ToDoApp.Domain/Entities/Note.cs
@@ -15,6 +15,8 @@
         public string Title { get; private set; } = null!;
         public string Content { get; private set; } = string.Empty;
         public bool IsDone { get; private set; } = false;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
         public User User { get; private set; } = null!;
         public int UserId { get; private set; }
 
@@ -24,16 +26,22 @@
                 throw new ArgumentException("Title cannot be null or empty.", nameof(title));
 
             Title = title;
+
+            UpdatedAt = DateTime.UtcNow;
         }
 
         public void UpdateContent(string content)
         {
             Content = content ?? string.Empty; // Allow content to be empty but not null
+
+            UpdatedAt = DateTime.UtcNow;
         }
 
         public void UpdateIsDone(bool isDone)
         {
             this.IsDone = isDone;
+
+            UpdatedAt = DateTime.UtcNow;
         }
     }
 }
