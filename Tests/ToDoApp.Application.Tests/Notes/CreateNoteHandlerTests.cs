fbb5c13f04191972e7bd33960fd655e8234d84b8
ArGul <argul.mail@gmail.com>
2026-07-23 15:16:48 +0300

    Created test for test tests system (lol).

---
+++ Tests/ToDoApp.Application.Tests/Notes/CreateNoteHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Application.Tests.Notes
+{
+    public class CreateNoteHandlerTests
+    {
+        [Fact]
+        public void Test_Should_Run()
+        {
+            Assert.True(true);
+        }
+    }
+}
