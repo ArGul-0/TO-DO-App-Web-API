f7eaacc61a2be2e66b998681a502ddb7ca270024
ArGul <argul.mail@gmail.com>
2026-08-12 13:51:21 +0300

    Created Tag Entity.

---
+++ ToDoApp.Domain/Entities/Tag.cs
@@ -0,0 +1,26 @@
+﻿namespace ToDoApp.Domain.Entities
+{
+    public class Tag
+    {
+        private Tag() { } // Private constructor for EF Core 
+
+        public Tag(string name, int ownerId)
+        {
+            this.Name = name;
+            this.OwnerId = ownerId;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; } = null!;
+        public int OwnerId { get; private set; }
+        public ICollection<NoteTag> NoteTags { get; private set; } = [];
+        
+        public void ChangeName( string newName )
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+                throw new ArgumentException("Tag name cannot be null, empty or whitespace.", nameof(newName));
+
+            this.Name = newName;
+        }
+    }
+}
