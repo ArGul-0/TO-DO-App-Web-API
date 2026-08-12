6ea3b58e59b161cd1fa88cf102b13a2c3442601c
ArGul <argul.mail@gmail.com>
2026-08-12 13:58:52 +0300

    Created NoteTag Entity.

---
+++ ToDoApp.Domain/Entities/NoteTag.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoApp.Domain.Entities
+{
+    public class NoteTag
+    {
+        private NoteTag() { } // Private constructor for EF Core
+        public NoteTag(int noteId, int tagId)
+        {
+            this.NoteId = noteId;
+            this.TagId = tagId;
+        }
+
+        public int NoteId { get; private set; }
+        public Note Note { get; private set; } = null!;
+        public int TagId { get; private set; }
+        public Tag Tag { get; private set; } = null!;
+    }
+}
