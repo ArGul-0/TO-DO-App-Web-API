683136e61cdbce6cf46ec103a6f15f2b18ccfca5
ArGul <argul.mail@gmail.com>
2026-08-21 00:09:57 +0300

    Realised basic TagRepository.

---
+++ ToDoApp.Infrastructure/Repositories/TagRepository.cs
@@ -0,0 +1,43 @@
+﻿using ToDoApp.Application.Interfaces.Repositories;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Infrastructure.Repositories
+{
+    internal class TagRepository : ITagRepository
+    {
+        public Task<Tag?> GetTagByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> GetTagByIdWithTrackingAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tag>> GetAllTagsWithOwnersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> GetTagWithOwnerByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tag>> GetAllTagsByUserIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddTagAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTagAsync(int tagId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
