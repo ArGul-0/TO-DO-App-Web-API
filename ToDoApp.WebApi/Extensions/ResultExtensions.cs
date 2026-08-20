cbaa3db7fd545cc1099d5a8d4274ada95be1b7ea
ArGul <argul.mail@gmail.com>
2026-08-21 00:28:00 +0300

    Now we use C# 14 feauter for extension everywhere.

--- ToDoApp.WebApi/Extensions/ResultExtensions.cs
+++ ToDoApp.WebApi/Extensions/ResultExtensions.cs
@@ -4,24 +4,27 @@ namespace ToDoApp.WebApi.Extensions
 {
     public static class ResultExtensions
     {
-        public static IResult ToHttpResult(this Result result)
+        extension(Result result)
         {
-            if (result.IsSuccess)
-                return Results.Ok();
-
-            return result.Error.Type switch
+            public IResult ToHttpResult()
             {
-                ErrorType.Validation => Results.BadRequest(result.Error),
-                ErrorType.NotFound => Results.NotFound(result.Error),
-                ErrorType.Unauthorized => Results.Unauthorized(),
-                ErrorType.Forbidden => Results.Json(result.Error, statusCode: 403),
-                ErrorType.Conflict => Results.Conflict(result.Error),
-                ErrorType.InternalServerError => Results.Problem(
-                                                    detail: result.Error.Description,
-                                                    statusCode: 500),
+                if (result.IsSuccess)
+                    return Results.Ok();
 
-                _ => Results.Problem(detail: result.Error.Description, statusCode: 500)
-            };
+                return result.Error.Type switch
+                {
+                    ErrorType.Validation => Results.BadRequest(result.Error),
+                    ErrorType.NotFound => Results.NotFound(result.Error),
+                    ErrorType.Unauthorized => Results.Unauthorized(),
+                    ErrorType.Forbidden => Results.Json(result.Error, statusCode: 403),
+                    ErrorType.Conflict => Results.Conflict(result.Error),
+                    ErrorType.InternalServerError => Results.Problem(
+                                                        detail: result.Error.Description,
+                                                        statusCode: 500),
+
+                    _ => Results.Problem(detail: result.Error.Description, statusCode: 500)
+                };
+            }
         }
 
         public static IResult ToHttpResult<T>(this ResultT<T> result)
