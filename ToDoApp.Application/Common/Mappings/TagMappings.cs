e4b327d426695c053cd0561a1c4be2afcec15c24
ArGul <argul.mail@gmail.com>
2026-08-20 23:59:32 +0300

    Created TagMappings.

---
+++ ToDoApp.Application/Common/Mappings/TagMappings.cs
@@ -0,0 +1,15 @@
+﻿using ToDoApp.Application.DTOs;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Application.Common.Mappings
+{
+    public static class TagMappings
+    {
+        public static TagDto ToDto(this Tag tag)
+        {
+            return new TagDto(
+                Name: tag.Name
+            );
+        }
+    }
+}
